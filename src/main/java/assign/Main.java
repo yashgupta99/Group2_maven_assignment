717abb05d3d3aa50a4fcb9984b9e0d0428f24d4b
yashgupta99 <yash.gupta99>
2021-01-14 12:58:06 +0530

    final commit for maven assignment

---
+++ src/main/java/assign/Main.java
@@ -0,0 +1,16 @@
+import assign.New.StringFunctions;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] k) {
+        Scanner ob = new Scanner(System.in);
+        System.out.print("Enter the string = ");
+        String str_in = ob.nextLine();
+        StringFunctions stringFunctions = new StringFunctions();
+        System.out.println("\nThe reverse of the string = " + stringFunctions.reverse(str_in));
+        System.out.println("The size of the string = " + stringFunctions.size(str_in));
+
+    }
+
+}
